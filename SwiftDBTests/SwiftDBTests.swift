a21db11a373c698130d741757b5d251bafb13afb
Remon Pel <remon@acato.nl>
2025-12-03 12:55:03 +0100

    Initial Commit

---
+++ SwiftDBTests/SwiftDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDBTests.swift
+//  SwiftDBTests
+//
+//  Created by Remon Pel on 03/12/2025.
+//
+
+import Testing
+@testable import SwiftDB
+
+struct SwiftDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
